1b5b7b68bf9d89f6f89d78557f68e8ff0ac3dfa6
tyrano <tyranosaurus@nate.com>
2021-04-27 20:52:33 +0900

    test(ValidatorTest.java): 유효성 체크 테스트
    숫자야구게임 기능 중 유효성 체크 기능을 테스트 한다.
    - validateStringNumbers() 에서 1~9 로 이루어진 서로 다른 3자리수 유효성 체크 테스트
    - validateStrikeAndBallCounts() 에서 스트라이크, 볼 개수 유효성 체크 테스트
    - validateRestartOrEndStatus() 에서 사용자 입력값에 따른 게임 재시작 또는 종료 상태 유효성 체크 테스트

--- src/test/java/baseballgame/BaseBallGameTest.java
+++ src/test/java/baseballgame/BaseBallGameTest.java
@@ -18,7 +18,7 @@ public class BaseBallGameTest {
 
 	@BeforeEach
 	void setUp() {
-		baseBallGame = new BaseBallGame();
+		baseBallGame = new BaseBallGame(new Validator());
 	}
 
 	@DisplayName("1~9 중 랜덤으로 숫자를 추출")
@@ -33,16 +33,6 @@ public class BaseBallGameTest {
 		assertThat(numberSet).contains(1, 2, 3, 4, 5, 6, 7, 8, 9)
 							 .size()
 							 .isEqualTo(9);
-	}
-
-	@DisplayName("1~9 로 이루어진 서로 다른 3자리수 유효성 체크")
-	@ParameterizedTest
-	@CsvSource(value = {"'':false", "012:false", "a23:false", "122:false", "1123:false", "3456:false", "123:true"}, delimiter = ':')
-	void validateStringNumbers(String value, boolean result) {
-		String[] stringNumbers = value.split("");
-		boolean validateResult = baseBallGame.validateStringNumbers(stringNumbers);
-
-		assertThat(validateResult).isEqualTo(result);
 	}
 
 	@DisplayName("스트라이크인 경우, 스트라이크 개수 증가")
@@ -61,7 +51,7 @@ public class BaseBallGameTest {
 
 	@DisplayName("볼인 경우, 볼 개수 증가")
 	@ParameterizedTest
-	@CsvSource(value = {"234:0", "612:0","621:1", "392:1", "169:2", "192:2", "196:3"}, delimiter = ':')
+	@CsvSource(value = {"234:0", "612:0", "621:1", "392:1", "169:2", "192:2", "196:3"}, delimiter = ':')
 	void increaseCountWhenBall(String stringNumber, int result) {
 		String[] problemNumbers = {"6", "1", "9"};
 		String[] answerNumbers = stringNumber.split("");
@@ -76,7 +66,7 @@ public class BaseBallGameTest {
 
 	@DisplayName("문제와 답이 맞는지 확인")
 	@ParameterizedTest
-	@CsvSource(value = {"234:false", "916:false", "169:false","691:false", "619:true"}, delimiter = ':')
+	@CsvSource(value = {"234:false", "916:false", "169:false", "691:false", "619:true"}, delimiter = ':')
 	void isCorrectAnswer(String stringNumber, boolean result) {
 		String[] problemNumbers = {"6", "1", "9"};
 		String[] answerNumbers = stringNumber.split("");
@@ -88,14 +78,5 @@ public class BaseBallGameTest {
 		}
 
 		assertThat(baseBallGame.isCorrectAnswer()).isEqualTo(result);
-	}
-
-	@DisplayName("게임 재시작 또는 종료 상태 유효성 체크")
-	@ParameterizedTest
-	@CsvSource(value = {"'':false", "abc:false", "a12!@#:false", "0:false", "1:true", "2:true"}, delimiter = ':')
-	void validateRestartOrEndStatus(String restartOrEndStatus, boolean result) {
-		boolean validate = baseBallGame.validateRestartOrEndStatus(restartOrEndStatus);
-
-		assertThat(validate).isEqualTo(result);
 	}
 }
---
+++ src/test/java/baseballgame/ValidatorTest.java
@@ -0,0 +1,48 @@
+package baseballgame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ValidatorTest {
+
+	private Validator validator;
+
+	@BeforeEach
+	void setUp() {
+		validator = new Validator();
+	}
+
+	@DisplayName("1~9 로 이루어진 서로 다른 3자리수 유효성 체크")
+	@ParameterizedTest
+	@CsvSource(value = {"'':false", "012:false", "a23:false", "122:false", "1123:false", "3456:false",
+		"123:true"}, delimiter = ':')
+	void validateStringNumbers(String value, boolean result) {
+		String[] stringNumbers = value.split("");
+		boolean validateResult = validator.validateStringNumbers(stringNumbers);
+
+		assertThat(validateResult).isEqualTo(result);
+	}
+
+	@DisplayName("스트라이크, 볼 개수 유효성 체크")
+	@ParameterizedTest
+	@CsvSource(value = {"2:2:false", "2:1:false", "3:1:false", "0:0:true", "1:0:true", "1:1:true", "2:0:true",
+		"0:3:true", "3:0:true"}, delimiter = ':')
+	void validateStrikeAndBallCounts(int strikeCount, int ballCount, boolean result) {
+		boolean validate = validator.validateStrikeAndBallCounts(strikeCount, ballCount);
+
+		assertThat(validate).isEqualTo(result);
+	}
+
+	@DisplayName("게임 재시작 또는 종료 상태 유효성 체크")
+	@ParameterizedTest
+	@CsvSource(value = {"'':false", "abc:false", "a12!@#:false", "0:false", "1:true", "2:true"}, delimiter = ':')
+	void validateRestartOrEndStatus(String restartOrEndStatus, boolean result) {
+		boolean validate = validator.validateRestartOrEndStatus(restartOrEndStatus);
+
+		assertThat(validate).isEqualTo(result);
+	}
+}
