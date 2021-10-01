d6a9fb146ad435c7eaa8531c4bc0277851545cbc
tyanotyrano <tyrano09@gmail.com>
2021-10-01 11:35:53 +0900

    test(HintTest.java): 볼 판정 기능 테스트
    볼 판정기능을 테스트한다.
    - HintTest : 볼 판정에 따른 볼 개수를 테스트

--- src/test/java/baseball/domain/HintTest.java
+++ src/test/java/baseball/domain/HintTest.java
@@ -27,4 +27,19 @@ class HintTest {
 		// then
 		assertEquals(hint.getStrikeCount(), strikeCount);
 	}
+
+	@DisplayName("볼인 경우, 볼 개수 증가한다")
+	@ParameterizedTest
+	@CsvSource(value = {"4:5:6:0", "4:5:1:1", "3:4:6:1", "3:5:1:2", "2:3:6:2", "2:3:1:3"}, delimiter = ':')
+	void increaseCountWhenBall(int firstNumber, int secondNumber, int thirdNumber, int ballCount) {
+		// given
+		BallNumbers problemBallNumbers = BallNumbers.createBy(new TestProblemNumberGenerator());
+		BallNumbers playerBallNumbers = BallNumbers.createBy(
+			new TestPlayerNumberGenerator(Arrays.asList(firstNumber, secondNumber, thirdNumber)));
+		// when
+		Hint hint = Hint.create();
+		hint.countBall(problemBallNumbers, playerBallNumbers);
+		// then
+		assertEquals(hint.getBallCount(), ballCount);
+	}
 }
